72976cce89c3182e7922c01ff5170812e00b4de1
Sebastian Kürten <sebastian@topobyte.de>
2022-09-15 12:23:53 +0200

    Let 'Send' accept optional message

--- cli/src/main/java/de/mobanisto/lanchat/Send.java
+++ cli/src/main/java/de/mobanisto/lanchat/Send.java
@@ -9,10 +9,15 @@ public class Send
 
 	public static void main(String[] args) throws IOException
 	{
+		String message = "LanChat host";
+		if (args.length != 0) {
+			message = args[0];
+		}
+
 		Sender sender = new Sender(5000);
 		List<InetAddress> addresses = sender.listAllBroadcastAddresses();
 		for (InetAddress address : addresses) {
-			sender.broadcast("LanChat host", address);
+			sender.broadcast(message, address);
 		}
 	}
 
