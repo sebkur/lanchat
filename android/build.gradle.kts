7376b60c485ba2eabe48056d166f7272c2aec078
Sebastian Kürten <sebastian@topobyte.de>
2022-10-06 09:50:40 +0200

    Configure debug build type with id suffix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,6 +18,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+        }
+    }
 }
 
 dependencies {
