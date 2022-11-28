c0b069cb7fc1a617b5241063425d88cc8c7e7e71
Sebastian Kürten <sebastian@topobyte.de>
2022-11-28 16:25:25 +0100

    Configure pinpit for packaging

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -1,32 +1,52 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-
 plugins {
-    kotlin("multiplatform")
+    id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.compose")
+    id("de.mobanisto.pinpit")
 }
 
-kotlin {
-    jvm {
-        withJava()
-    }
-    sourceSets {
-        named("jvmMain") {
-            dependencies {
-                implementation(compose.desktop.currentOs)
-                implementation(project(":lanchat-core"))
-                implementation(project(":lanchat-compose"))
-            }
+val attributeUsage = Attribute.of("org.gradle.usage", String::class.java)
+
+val currentOs: Configuration by configurations.creating {
+    extendsFrom(configurations.implementation.get())
+    attributes { attribute(attributeUsage, "java-runtime") }
+}
+
+val windowsX64: Configuration by configurations.creating {
+    extendsFrom(configurations.implementation.get())
+    attributes { attribute(attributeUsage, "java-runtime") }
+}
+
+val linuxX64: Configuration by configurations.creating {
+    extendsFrom(configurations.implementation.get())
+    attributes { attribute(attributeUsage, "java-runtime") }
+}
+
+sourceSets {
+    main {
+        java {
+            compileClasspath = currentOs
+            runtimeClasspath = currentOs
         }
     }
 }
 
-compose.desktop {
+dependencies {
+    implementation(project(":lanchat-core"))
+    implementation(project(":lanchat-compose"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    currentOs(compose.desktop.currentOs)
+    windowsX64(compose.desktop.windows_x64)
+    linuxX64(compose.desktop.linux_x64)
+}
+
+pinpit.desktop {
     application {
         mainClass = "de.mobanisto.lanchat.LanChatKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Deb)
+            jvmVendor = "adoptium"
+            jvmVersion = "17.0.5+8"
+
             packageName = "Lanchat"
             description = "Lanchat - Insecure Network Chat"
             vendor = "Mobanisto"
@@ -34,9 +54,18 @@ compose.desktop {
             licenseFile.set(project.file("LICENSE.txt"))
             linux {
                 packageName = "lanchat"
+                debMaintainer = "sebastian@mobanisto.de"
                 debPackageVersion = "0.0.1"
                 appCategory = "comm"
-                menuGroup = "Network;Chat;InstantMessaging;"
+                menuGroup = "Network;Chat;InstantMessaging"
+                deb("UbuntuFocalX64") {
+                    qualifier = "ubuntu-20.04"
+                    arch = "x64"
+                    depends(
+                        "libc6", "libexpat1", "libgcc-s1", "libpcre3", "libuuid1", "xdg-utils",
+                        "zlib1g", "libnotify4"
+                    )
+                }
             }
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         val kotlinVersion = extra["kotlin.version"] as String
         val agpVersion = extra["agp.version"] as String
         val composeVersion = extra["compose.version"] as String
+        val pinpitVersion = extra["pinpit.version"] as String
 
         kotlin("jvm").version(kotlinVersion)
         kotlin("multiplatform").version(kotlinVersion)
@@ -16,6 +17,7 @@ pluginManagement {
         id("com.android.application").version(agpVersion)
         id("com.android.library").version(agpVersion)
         id("org.jetbrains.compose").version(composeVersion)
+        id("de.mobanisto.pinpit").version(pinpitVersion)
     }
 }
 
