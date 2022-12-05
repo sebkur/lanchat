4afe7da4673bb245ec89473fad28d4769b47a3f8
Sebastian Kürten <sebastian@topobyte.de>
2022-12-05 15:34:26 +0100

    Create ico with subimages of size 16, 32, 48 and 256

--- artwork/convert.sh
+++ artwork/convert.sh
@@ -6,6 +6,10 @@ inkscape -C -e dialog.png dialog.svg
 convert banner.png ../desktop/src/main/packaging/windows/banner.bmp
 convert dialog.png ../desktop/src/main/packaging/windows/dialog.bmp
 
-convert icon128.png ../desktop/src/main/packaging/windows/lanchat.ico
+inkscape -C -e icon16.png -h 16 icon.svg
+inkscape -C -e icon32.png -h 32 icon.svg
+inkscape -C -e icon48.png -h 48 icon.svg
+inkscape -C -e icon256.png -h 256 icon.svg
+convert icon16.png icon32.png icon48.png icon256.png ../desktop/src/main/packaging/windows/lanchat.ico
 
 inkscape -C -h 500 -e ../desktop/src/main/packaging/deb/lanchat.png icon.svg
