62bfc1f5c5c0d9f14ad3332f06ad4fe0bbd7c8f4
Sebastian Kürten <sebastian@topobyte.de>
2023-08-22 12:04:51 +0200

    Increment version code and name

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     defaultConfig {
         minSdk = 21
         targetSdk = 33
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
     }
 
     compileOptions {
