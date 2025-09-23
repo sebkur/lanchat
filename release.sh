915cc440474ef1b99b375e5abfbe561fd60893ba
Sebastian Kürten <sebastian@topobyte.de>
2025-09-23 09:03:11 +0200

    Add script for producing release

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+VERSION=$(grep '^pVersion=' gradle.properties | cut -d'=' -f2)
+echo "Building release $VERSION"
+
+./gradlew clean
+
+# macOS
+./gradlew \
+    pinpitPackageDefaultDistributableZipMacosX64 \
+    pinpitPackageDefaultDistributableZipMacosArm64 \
+
+# Windows
+./gradlew \
+    pinpitPackageDefaultMsiX64
+
+# Linux
+./gradlew \
+    pinpitPackageDefaultDistributableTarGzLinuxX64 \
+    pinpitPackageDefaultDebUniversalX64
+
+DIR=dist
+
+rm -rf $DIR
+mkdir $DIR
+
+FILES=$(find desktop/build -type f -and \( \
+    -name "*.zip" -or -name "*msi" -or \
+    -name "*.deb" -or -name "*.tar.gz" \))
+
+for f in $FILES; do
+    echo $f;
+    cp $f $DIR;
+done;
+
+mv "$DIR/Lanchat-x64-$VERSION.zip"           "$DIR/Lanchat-macos-x64-$VERSION.zip"
+mv "$DIR/Lanchat-arm64-$VERSION.zip"         "$DIR/Lanchat-macos-arm64-$VERSION.zip"
+
+mv "$DIR/Lanchat-x64-$VERSION.msi"           "$DIR/Lanchat-windows-x64-$VERSION.zip"
+
+mv "$DIR/lanchat-universal-x64-$VERSION.deb" "$DIR/Lanchat-x64-$VERSION.deb"
+mv "$DIR/lanchat-x64-$VERSION.tar.gz"        "$DIR/Lanchat-linux-x64-$VERSION.tar.gz"
