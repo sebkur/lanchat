f128876214b4ce90f809ae88289fa1ddb4ccf89f
Sebastian Kürten <sebastian@topobyte.de>
2025-09-23 10:31:38 +0200

    Fix build problems for Android due to JVM target versions
    Apparently AGP 8.X runs on JDK 17 and support Java 17 language features
    and seems to require source and target compatibility to be set to 17 for
    the android modules. It seems to have worked before but currently the
    project did no longer build.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,12 +19,12 @@ val rootVersionCode = version as String
 val androidVersionCode = (extra["androidVersionCode"] as String).toInt()
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 android {
@@ -39,8 +39,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     if (haveKeystoreProperties) {
